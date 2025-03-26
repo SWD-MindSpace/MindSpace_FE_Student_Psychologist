9d5436f7d020b1d4bbace91e5b427e579d058989
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-03-26 20:09:13 +0700

    added psychologist manage schedule

---
+++ src/app/schedules/page.tsx
@@ -0,0 +1,6 @@
+// pages/manage-timeslots.tsx
+import TimeSlotCalendar from "../../components/schedule/TimeSlotCalendar";
+
+export default function ManageTimeslots() {
+    return <TimeSlotCalendar />;
+}
